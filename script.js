bfb99e6a9d279688c3cb3c2fb12d3321b4e3e718
bichitrabehera <bichitrabehera.amcec@gmail.com>
2025-02-27 22:07:29 +0530

    animations

--- script.js
+++ script.js
@@ -52,3 +52,90 @@ document.addEventListener("click", function (event) {
     }
 });
 
+
+var tl = gsap.timeline()
+
+tl.from(".logo", {
+    y: -50,
+    scrub: 0.3,
+    stagger: 0.1,
+    duration: 0.5
+})
+
+tl.from(".right li", {
+    y: -50,
+    scrub: 0.3,
+    stagger: 0.1,
+    duration: 0.5
+})
+
+tl.from(".toggle-btn", {
+    y: -50,
+    scrub: 0.3,
+    stagger: 0.1,
+    duration: 0.5
+}, "=-1")
+
+tl.from(".heading-about", {
+    x: -1000,
+    duration: 0.5,
+    scrub: 0.3,
+    stagger: 0.1,
+    duration: 0.5
+}, "=-1");
+
+tl.from(".homeabout", {
+    x: -1000,
+    opacity: 0,
+    scrub: 0.3,
+    stagger: 0.1,
+    duration: 0.5
+}, "=-1")
+
+tl.from(".heroo-btn", {
+    opacity: 0,
+    scrub: 0.3,
+    stagger: 0.1,
+    duration: 0.5
+})
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    gsap.registerPlugin(ScrollTrigger);
+
+    function animateSection(target, animationProps) {
+        gsap.from(target, {
+            ...animationProps,
+            scrollTrigger: {
+                trigger: target,
+                start: "top 100%", // When 85% of the element is visible
+                end: "top 80%",  // Reverse when 20% is visible
+                toggleActions: "play reverse play reverse",
+                scrub: 1, // Smooth scrolling effect
+                markers: false, // Set to true for debugging
+            },
+        });
+    }
+    gsap.utils.toArray(".titles").forEach((title, index) => {
+        animateSection(title, { opacity: 0, y: 50, duration: 1, delay: index * 0.1 });
+    });
+    gsap.utils.toArray(".underline").forEach((underline, index) => {
+        animateSection(underline, { width: "0%", duration: 1, delay: index * 0.1 });
+    });
+    animateSection(".about-info", { opacity: 0, y: 50, duration: 1 });
+    animateSection(".aboutcont", { opacity: 0, x: -50, duration: 1, stagger: 0.3 });
+    animateSection(".skills-vert", { opacity: 0, x: 50, duration: 1, stagger: 0.3 });
+    gsap.utils.toArray(".rowskill h2").forEach((skill, index) => {
+        animateSection(skill, { opacity: 0, y: 30, duration: 1, delay: index * 0.1 });
+    });
+    gsap.utils.toArray(".project").forEach((project, index) => {
+        animateSection(project, { opacity: 0, y: 50, duration: 1, delay: index * 0.2 });
+    });
+    animateSection(".contact-section", { opacity: 0, y: 50, duration: 1 });
+    gsap.utils.toArray("#contactForm input, #contactForm textarea, .contact-btn").forEach((field, index) => {
+        animateSection(field, { opacity: 0, y: 30, duration: 1, delay: index * 0.1 });
+    });
+    animateSection(".footer-name", { opacity: 0, x: -50, duration: 1 });
+    animateSection(".social-links", { opacity: 0, x: 50, duration: 1 });
+
+});
